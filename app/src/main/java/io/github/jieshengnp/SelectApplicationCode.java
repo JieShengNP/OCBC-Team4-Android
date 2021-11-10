b6cb5de9d5bea090a50484185534a5dc054c3403
JieShengNP <s10205049@connect.np.edu.sg>
2021-11-10 12:46:17 +0800

    Add SelectApplicationType

--- app/src/main/java/io/github/jieshengnp/SelectApplicationCode.java
+++ app/src/main/java/io/github/jieshengnp/SelectApplicationCode.java
@@ -2,13 +2,28 @@ package io.github.jieshengnp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectApplicationCode extends AppCompatActivity {
+
+    Button noAppCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_application_code);
+
+        noAppCode = findViewById(R.id.noAppCode);
+
+        noAppCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SelectApplicationCode.this, SelectApplicationType.class);
+                startActivity(i);
+            }
+        });
     }
 }
