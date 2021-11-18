432aed9bae899fb41c72f37a480affe8ecbcfb31
herman088 <hermancho08@gmail.com>
2021-11-19 01:56:54 +0800

    Remove Image IC verification from COnfirmPage and Redo Confirm Page

--- app/src/main/java/io/github/jieshengnp/ConfirmationPage.java
+++ app/src/main/java/io/github/jieshengnp/ConfirmationPage.java
@@ -31,12 +31,10 @@ import java.io.ByteArrayOutputStream;
 
 public class ConfirmationPage extends AppCompatActivity {
     ProgressBar progressbar4,progressbar5,progressbar6;
-    ImageView uploadedSelfie;
-    Button uploadSelfie,confirmButton;
-    ActivityResultLauncher<Intent> activityResultLauncher;
-    Bitmap imageBitmap;
-    private static final int REQUEST_CAMERA_CODE = 100;
-    StorageReference storageRef = FirebaseStorage.getInstance("gs://ocbc-team4-2b3ee.appspot.com/").getReference();
+
+
+
+    Button accCreate1,accCreate2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,80 +42,22 @@ public class ConfirmationPage extends AppCompatActivity {
         setContentView(R.layout.activity_confirmation_page);
         progressbar4= findViewById(R.id.progressBar4);
         progressbar5 = findViewById(R.id.progressBar5);
-        uploadedSelfie = findViewById(R.id.imageViewSelfie);
-        uploadSelfie = findViewById(R.id.uploadSelfie);
-        confirmButton = findViewById(R.id.uploadConfirmm);
         progressbar4.setProgress(100);
         progressbar5.setProgress(100);
 
+        accCreate1=findViewById(R.id.accCreate1);
+        accCreate2=findViewById(R.id.accCreate2);
 
-        if (ContextCompat.checkSelfPermission(ConfirmationPage.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(ConfirmationPage.this, new String[]{
-                    Manifest.permission.CAMERA
-
-
-            }, REQUEST_CAMERA_CODE);
-
-        }
-
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                    Bundle extras = result.getData().getExtras();
-                    imageBitmap = (Bitmap) extras.get("data");
-                    uploadedSelfie.setImageBitmap(imageBitmap);
+        Intent receivIntent = getIntent();
 
 
-                }
-
-            }
-        });
-
-        uploadSelfie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    activityResultLauncher.launch(intent);
-                } else {
-                    Toast.makeText(ConfirmationPage.this, "Cant Open",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,bytes);
-                byte[] bb = bytes.toByteArray();
-                uploadFirebase(bb);
-            }
-        });
-
-    }
-
-    private void uploadFirebase(byte[]bb){
-
-        StorageReference imagePath = storageRef.child("verificationImgs/test.jpg");
-        UploadTask uploadTask = imagePath.putBytes(bb);
-        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(ConfirmationPage.this,"Sucessfully Uploaded Picture",Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ConfirmationPage.this,"Failed To Upload Picture",Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
 
 
     }
+
+
+
+
 }
