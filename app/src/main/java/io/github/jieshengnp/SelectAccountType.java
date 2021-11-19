bfc04b249f71ed0b0860e8ec5336b37bb9b7ece4
JieShengNP <s10205049@connect.np.edu.sg>
2021-11-19 09:19:58 +0800

    Update AccountType "Button" Style

--- app/src/main/java/io/github/jieshengnp/SelectAccountType.java
+++ app/src/main/java/io/github/jieshengnp/SelectAccountType.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SelectAccountType extends AppCompatActivity {
 
-    Button individualAccBtn, jointAccBtn;
+    TextView individualAccBtn, jointAccBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
