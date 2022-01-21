aabf092e67945f1e3b2ec61219f19040386bf05e
Mxrcxsz <s10208495@connect.np.edu.sg>
2022-01-21 19:44:12 +0800

    Adding in biometrics

--- app/src/main/java/io/github/jieshengnp/SelectAccountType.java
+++ app/src/main/java/io/github/jieshengnp/SelectAccountType.java
@@ -1,12 +1,11 @@
 package io.github.jieshengnp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SelectAccountType extends AppCompatActivity {
 
